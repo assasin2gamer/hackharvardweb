2980e18c73d64d0f7d8d0459a40479d0f4551d61
Stephen Chang <sjchang@Stephens-MacBook-Air.local>
2023-10-21 02:41:08 -0400

    added website

--- src/App.js
+++ src/App.js
@@ -2,22 +2,70 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  let blink_counter = 0;
+  let space_counter = 0;
+
+
+  const words = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+
+  const wordItems = words.map((word, index) => {
+    return <li  key={index}>{word}</li>;
+  });
+
+  const category = words.map((word, index) => {
+    return <li key={index}>{word}</li>;
+  });
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex', width: '100%', height: '30%', marginTop: '10%' }}>
+
+        <div style={{ width: '45%', borderStyle: 'solid', marginRight: '5%', marginLeft: '2.5%', height: '30vh' }}>
+          <div>
+            Blink Counter
+
+          </div>
+          <div style={{fontSize:'100px'}}>
+            {blink_counter}
+          </div>
+        </div>
+        <div style={{ width: '45%', borderStyle: 'solid', marginRight: '5%', marginLeft: '2.5%', height: '30vh' }}>
+          <div>
+            space_counter
+          </div>
+          <div style={{fontSize:'100px'}}>
+            {space_counter}
+          </div>
+        </div>
+      </div>
+
+
+      <div style={{ display: 'flex', width: '100%', height: '30%', marginTop: '5%' }}>
+        <div style={{ width: '45%', borderStyle: 'solid', marginRight: '5%', marginLeft: '2.5%', height: '30vh' }}>
+          <div>
+            category
+          </div>
+          <div>
+            {category}
+          </div>
+        </div>
+        <div style={{ width: '45%', borderStyle: 'solid', marginRight: '5%', marginLeft: '2.5%', height: '30vh' }}>
+          <div>
+            words
+          </div>
+          <div>
+            {wordItems}
+          </div>
+        </div>
+      </div>
+
+      <div>
+      </div>
     </div>
   );
 }
