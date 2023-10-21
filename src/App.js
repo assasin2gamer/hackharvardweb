fb315c333ffa45f195565ef1d893ae97ea0bea23
Nureldin-Mohamed <nureldinshokrymohamed@gmail.com>
2023-10-21 09:11:22 -0400

    update uwu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import React, {useEffect} from "react";
+import Granim from "granim";
 
 function App() {
 
@@ -18,53 +21,78 @@ function App() {
     return <li key={index}>{word}</li>;
   });
 
+  useEffect(() => {
+    new Granim({
+      element: "#gradient-canvas",
+      direction: "left-right",
+      opacity: [1,1],
+      states: {
+        "default-state": {
+          gradients: [
+            ["#040147", "#085701"],
+            ["#b50297", "#b00239"],
+            ["#c96100", "#057c87"],
+            ["#b00239", "#b50297"],
+            ["#057c87", "#c96100"],
+            ["#b50297", "#b00239"],
+          ],
+          transitionSpeed: 5000,
+        },
+      },
+    });
+  }, []);
+
 
 
 
   return (
     <div className="App">
-      <div style={{ display: 'flex', width: '100%', height: '30%', marginTop: '10%' }}>
+      <canvas id="gradient-canvas"/>
+      <div id="gradient-mask"/>
 
-        <div style={{ width: '45%', borderStyle: 'solid', marginRight: '5%', marginLeft: '2.5%', height: '30vh' }}>
-          <div>
-            Blink Counter
+        <div id = "left" style={{ display: 'flex', width: '100%', height: '30%', marginTop: '5%' }}>
+          <div style={{ width: '45%', borderStyle: 'solid', marginRight: '5%', marginLeft: '2.5%', height: '30vh', borderColor: 'white', backgroundColor: "rgba(0,0,0,0.5)", borderWidth: '0.3vh', borderRadius: '4vh', height: '80vh'}}>
+            
+            <div style={{color: "white"}}>
+              category
+            </div>
 
-          </div>
-          <div style={{fontSize:'100px'}}>
-            {blink_counter}
+            <div style={{color: "white"}}>
+              {category}
+            </div>
+
           </div>
         </div>
-        <div style={{ width: '45%', borderStyle: 'solid', marginRight: '5%', marginLeft: '2.5%', height: '30vh' }}>
-          <div>
-            space_counter
-          </div>
-          <div style={{fontSize:'100px'}}>
-            {space_counter}
-          </div>
-        </div>
-      </div>
 
+        <div id = "right">
 
-      <div style={{ display: 'flex', width: '100%', height: '30%', marginTop: '5%' }}>
-        <div style={{ width: '45%', borderStyle: 'solid', marginRight: '5%', marginLeft: '2.5%', height: '30vh' }}>
           <div>
-            category
-          </div>
-          <div>
-            {category}
-          </div>
-        </div>
-        <div style={{ width: '45%', borderStyle: 'solid', marginRight: '5%', marginLeft: '2.5%', height: '30vh' }}>
-          <div>
-            words
-          </div>
-          <div>
-            {wordItems}
-          </div>
-        </div>
-      </div>
+            
+            <div id="top" style={{ width: '45%', borderStyle: 'solid', margin: 'auto', height: '30vh', borderColor: 'white', backgroundColor: "rgba(0,0,0,0.5)", borderWidth: '0.3vh', borderRadius: '4vh', position: "relative"}}>
+              
+              <div style={{color: "white"}}>
+                Blink Counter
+              </div>
 
-      <div>
+              <div style={{fontSize:'100px', color: "white"}}>
+                {blink_counter}
+              </div>
+
+              <div style={{ width: '45%', borderStyle: 'solid', margin: 'auto', height: '30vh', borderColor: 'white', backgroundColor: "rgba(0,0,0,0.5)", borderWidth: '0.3vh', borderRadius: '4vh', position: "absolute"}}>
+              
+                <div style={{color: "white"}}>
+                  space_counter
+                </div>
+
+                <div style={{fontSize:'100px', color: "white"}}>
+                  {space_counter}
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+
       </div>
     </div>
   );
